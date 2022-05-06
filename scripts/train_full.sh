a0b376faf8175045bfa031960b5f3d9c66f78d03
MiuGod0126 <68314259+MiuGod0126@users.noreply.github.com>
2022-05-06 08:55:16 +0800

    Update train_full.sh

--- scripts/train_full.sh
+++ scripts/train_full.sh
@@ -5,6 +5,6 @@ python paddleseq_cli/train.py --cfg configs/zhen_ccmt.yaml \
                          --update-freq 4 \
                          --max-epoch 10 \
                          --save-epoch 1 \
-                         --save-dir /root/paddlejob/workspace/output \
+                         --save-dir output \
                          --log-steps 100 \
                          --max-tokens 4096
